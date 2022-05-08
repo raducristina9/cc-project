1b7d3a3f13798b10c5dd7fa9072f1ee809c8f028
crist <raducristina18@stud.ase.ro>
2022-05-08 17:46:25 +0300

    added meetinginfo v1

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require("body-parser");
 const utilsRouter = require('./router/utilsRouter');
+const messagesRouter = require('./router/messagesRouter');
 
 const app = express();
 app.use(cors());
@@ -13,6 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.use("/utils", utilsRouter);
+app.use("/messages", messagesRouter);
 
 const port = process.env.PORT || 3001;
 
