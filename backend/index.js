59d002a1d5c9422635a6454703e365742a9811eb
crist <raducristina18@stud.ase.ro>
2022-05-08 14:15:42 +0300

    added sendgrid api

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,21 @@
-const express = require('express')
-const cors = require('cors')
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require("body-parser");
+const utilsRouter = require('./router/utilsRouter');
+
 const app = express();
-app.use(cors())
+app.use(cors());
+
+// for parsing application/json
+app.use(bodyParser.json()); 
+// for parsing application/xwww-
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.use("/utils", utilsRouter);
 
 const port = process.env.PORT || 3001;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-});
-
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`)
+    console.log(`Cloud computing app listening on port ${port}!`)
 });
